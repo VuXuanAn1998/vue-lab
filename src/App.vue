3790798065cbe699f9b944f4d40c2264a353b154
vuxuanan1998 <vuxuanan1998@gmail.com>
2024-05-25 22:24:06 +0700

    setup store

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{ messageCount }}
+    <button @click="increment">click me</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapMutations } from 'vuex';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  computed: {
+    ...mapState({
+      messageCount: (state) => state.user.user,
+    })
+  },
+  methods: {
+    ...mapMutations(['increment'])
   }
 }
 </script>
