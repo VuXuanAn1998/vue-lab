dca2c5a0128535875dd05d06478dbd114e9f225e
vuxuanan1998 <vuxuanan1998@gmail.com>
2024-05-26 09:57:07 +0700

    setup ts and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <div>
-
-    <img alt="Vue logo" src="./assets/logo.png">
-    {{ messageCount }}
-    <button @click="increment">click me</button>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import { mapState, mapMutations } from 'vuex';
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
   name: 'App',
   components: {
-  },
-  computed: {
-    ...mapState({
-      messageCount: (state) => state.user.user,
-    })
-  },
-  methods: {
-    ...mapMutations(['increment'])
+    HelloWorld
   }
-}
+});
 </script>
 
 <style>
